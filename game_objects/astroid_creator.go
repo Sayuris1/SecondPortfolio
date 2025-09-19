93a8e501a489e7c11f25873e9026cb3122fae5c5
DESKTOP-T5QHUCS\Admin <elgunalp1@gmail.com>
2025-09-19 21:56:57 +0300

    astroid creator

---
+++ game_objects/astroid_creator.go
@@ -0,0 +1,34 @@
+components {
+  id: "astroid_creator"
+  component: "/scripts/astroid_creator.script"
+}
+embedded_components {
+  id: "factory5"
+  type: "factory"
+  data: "prototype: \"/assets/models/astroids/astroid5.go\"\n"
+  ""
+}
+embedded_components {
+  id: "factory1"
+  type: "factory"
+  data: "prototype: \"/assets/models/astroids/astroid1.go\"\n"
+  ""
+}
+embedded_components {
+  id: "factory2"
+  type: "factory"
+  data: "prototype: \"/assets/models/astroids/astroid2.go\"\n"
+  ""
+}
+embedded_components {
+  id: "factory3"
+  type: "factory"
+  data: "prototype: \"/assets/models/astroids/astroid3.go\"\n"
+  ""
+}
+embedded_components {
+  id: "factory4"
+  type: "factory"
+  data: "prototype: \"/assets/models/astroids/astroid4.go\"\n"
+  ""
+}
